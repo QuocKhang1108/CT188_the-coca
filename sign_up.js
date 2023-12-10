6ecfae922ac6d3efa24ef702add3d383c450bdb0
QuocKhang <phamquockhang1108@gmail.com>
2023-12-10 13:14:24 +0700

    after registering, redirected to the login

--- sign_up.js
+++ sign_up.js
@@ -34,27 +34,25 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
 
     if (validateForm()) {
-
       const email = emailInput.value;
       const password = passwordInput.value;
 
-      
       if (!isEmailUsed(email)) {
         const user = { email, password };
         saveUser(email, user);
-        alert('Đăng kí thành công!');
+        alert("Đăng kí thành công!");
+        window.location.href = "log_in.html";
       } else {
-        alert('Địa chỉ email đã được sử dụng!');
+        alert("Địa chỉ email đã được sử dụng!");
       }
     }
   });
 
-
-  firstNameInput.addEventListener('blur', validateFirstName);
-  lastNameInput.addEventListener('blur', validateLastName);
-  emailInput.addEventListener('blur', validateEmail);
-  passwordInput.addEventListener('blur', validatePassword);
-  passwordConfirmInput.addEventListener('blur', validatePasswordConfirm);
+  firstNameInput.addEventListener("blur", validateFirstName);
+  lastNameInput.addEventListener("blur", validateLastName);
+  emailInput.addEventListener("blur", validateEmail);
+  passwordInput.addEventListener("blur", validatePassword);
+  passwordConfirmInput.addEventListener("blur", validatePasswordConfirm);
 
   firstNameInput.addEventListener("focus", hideErrorMessage);
   lastNameInput.addEventListener("focus", hideErrorMessage);
@@ -78,18 +76,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     return validateInput(
       firstNameInput,
-      'First Name is required',
+      "First Name is required",
       firstNameRegex.test(firstNameValue)
     );
   }
 
   function validateLastName() {
     const lastNameValue = lastNameInput.value.trim();
-    const lastNameRegex = /^[a-zA-Z]+$/; 
+    const lastNameRegex = /^[a-zA-Z]+$/;
 
     return validateInput(
       lastNameInput,
-      'Last Name is required',
+      "Last Name is required",
       lastNameRegex.test(lastNameValue)
     );
   }
@@ -109,23 +107,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
-  
 
   function validatePassword() {
-
     const passwordValue = passwordInput.value.trim();
     // password: ít nhất 8 ký tự, chứa ít nhất một chữ cái và một chữ số
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
     return validateInput(
       passwordInput,
-      'Password is required and should be at least 8 characters',
+      "Password is required and should be at least 8 characters",
       passwordRegex.test(passwordValue)
     );
-
   }
-
-  
 
   function validatePasswordConfirm() {
     const passwordValue = passwordInput.value.trim();
@@ -140,25 +133,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-
   function validateInput(input, message, isValid) {
     const formControl = input.parentElement;
-    const errorElement = formControl.querySelector('span.error-message');
-  
+    const errorElement = formControl.querySelector("span.error-message");
+
     if (!isValid) {
-      formControl.classList.remove('success');
-      formControl.classList.add('error');
+      formControl.classList.remove("success");
+      formControl.classList.add("error");
       errorElement.innerText = message;
-      errorElement.style.color = 'white';
+      errorElement.style.color = "white";
       return false;
     } else {
-      formControl.classList.remove('error');
-      formControl.classList.add('success');
-      errorElement.innerText = ''; 
+      formControl.classList.remove("error");
+      formControl.classList.add("success");
+      errorElement.innerText = "";
       return true;
     }
   }
-  
 
   function showError(input, message) {
     const formControl = input.parentElement;
@@ -179,7 +170,6 @@ document.addEventListener("DOMContentLoaded", function () {
     return true;
   }
 
-
   function hideErrorMessage(event) {
     const input = event.target;
     const formControl = input.parentElement;
@@ -189,15 +179,12 @@ document.addEventListener("DOMContentLoaded", function () {
     errorElement.innerText = "";
   }
 
-
   function isEmailUsed(email) {
     const storedUser = localStorage.getItem(email);
-    return !!storedUser; 
+    return !!storedUser;
   }
 
   function saveUser(email, user) {
     localStorage.setItem(email, JSON.stringify(user));
   }
-  
-
 });
